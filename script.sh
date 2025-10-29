8a96598731ed1dd8357b0a3b30969de8daab440b
mustafa07072002 <mustafamahgoub.2002@gmail.com>
2025-10-29 18:29:45 +0300

    Changed File Directories

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ~/myDirectory/secondDirectory -p
-touch ~/myDirectory/secondDirectory/myNotePaper.txt
-cp ~/myDirectory/secondDirectory/myNotePaper.txt ~/myDirectory/
-mv ~/myDirectory/myNotePaper.txt ~/myDirectory/myOldNotePaper.txt
